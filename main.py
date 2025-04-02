60cda119eb45f898ec57df837369998a96b50103
VladimirAkb <akbreal77@gmail.com>
2025-04-02 23:55:22 +0300

    DZ files

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import random
-
-# Ввод диапазона
-lower_bound = int(input("Введите нижнюю границу диапазона: "))
-upper_bound = int(input("Введите верхнюю границу диапазона: "))
-
-# Генерация случайного числа в заданном диапазоне
-random_number = random.randint(lower_bound, upper_bound)
-
-# Вывод случайного числа
-print(f"Случайное число в диапазоне от {lower_bound} до {upper_bound}: {random_number}")
+def area_circle(r):
+    s = 3.14 * r ** 2 
+    return s
